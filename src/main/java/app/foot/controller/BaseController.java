8f4e36cbb8193ea3a1a75acfe1d4442f360ddfd3
Sarobidy-23 <hei.sarobidy@gmail.com>
2023-02-08 20:59:06 +0300

    chore: add exception handler

--- src/main/java/app/foot/controller/BaseController.java
+++ src/main/java/app/foot/controller/BaseController.java
@@ -1,7 +1,10 @@
 package app.foot.controller;
 
 import app.foot.exception.BadRequestException;
+import app.foot.exception.InternalServerException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class BaseController {
 
     @ExceptionHandler({BadRequestException.class})
-    public void handleException() {
+    public ResponseEntity<BadRequestException> handleException(BadRequestException e) {
         log.info("A bad request exception was handled");
+        return new ResponseEntity<>(e, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler({InternalServerException.class})
+    public ResponseEntity<InternalServerException> handleException(InternalServerException e) {
+        log.info("A internal server request exception was handled");
+        return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
