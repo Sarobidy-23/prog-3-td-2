8e383d6e7000f38a5f796f055196f93fe085b4d2
Sarobidy-23 <hei.sarobidy@gmail.com>
2023-02-09 09:33:31 +0300

    feat: update exception handler

--- src/main/java/app/foot/controller/BaseController.java
+++ src/main/java/app/foot/controller/BaseController.java
@@ -5,10 +5,11 @@ import app.foot.exception.InternalServerException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@ControllerAdvice
 @Slf4j
 public class BaseController {
 
